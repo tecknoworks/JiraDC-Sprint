b76daf7ab35e77bc17576702d488f858517d43e4
Damaris Virtan <virtan.alexandra@gmail.com>
2021-08-16 17:18:04 +0300

    feat: addSprint

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const Sprint = require('./models/sprint')
 
 const port = 8090
 
-var mongoDB = 'mongodb+srv://Damaris:12345@cluster0.gdp4f.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+var mongoDB = 'mongodb+srv://cata:cata@cluster0.wcbqw.mongodb.net/first?retryWrites=true&w=majority';
 mongoose.connect(mongoDB, {useNewUrlParser: true, useUnifiedTopology: true});
 
 var db = mongoose.connection;
@@ -20,7 +20,7 @@ db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
 app.post('/sprint', async (req, res) => {
     let newSprint = req.body
-    var addSprint=new Sprint({name:newSprint.name,project_id:newSprint.project_id})
+    var addSprint=new Sprint({name:newSprint.name,description:newSprint.description,project_id:newSprint.project_id})
     await Sprint.create(addSprint)
     res.send(newSprint)
 })
@@ -29,21 +29,6 @@ app.get('/sprint', async (req, res) =>{
     // const record= await Project.find({'type':req.query.type}).exec()
     const record= await Sprint.find({})
     console.log(record)
-    res.json(record)
-})
-
-app.get('/project/kanban', async (req, res) =>{
-    const record= await Project.find({'type':'kanban'}).exec()
-    console.log(record)
-    res.json(record)
-})
-app.get('/project/scrum', async (req, res) =>{
-    const record= await Project.find({'type':'scrum'}).exec()
-    res.json(record)
-})
-app.get('/project/bugtracking', async (req, res) =>{
-
-    const record= await Project.find({'type':'bug tracking '}).exec()
     res.json(record)
 })
 
